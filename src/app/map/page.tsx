6b710986081060e62417e5dea8fb6936c2ca4ba7
Dennis <dennis@momentum.local>
2023-12-05 13:01:30 -0800

    add '/map' using maplibre

---
+++ src/app/map/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+import Map, {
+  GeolocateControl,
+  NavigationControl,
+} from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+export default function MapComponent() {
+  return (
+    <Map
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14,
+      }}
+      style={{
+        height: "100vh",
+        width: "100vw",
+        position: "fixed",
+        overflow: "hidden",
+      }}
+      mapStyle="https://tiles.basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
+    >
+      <GeolocateControl position="top-right" />
+      <NavigationControl position="top-right" />
+    </Map>
+  );
+}
